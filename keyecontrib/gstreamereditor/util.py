8a93d7c4d76a3965215d54d4b073974b42853f98
jv_203_lubuntu <leef918@gmail.com>
2023-06-07 13:48:15 +0700

    sort plugins id

--- keyecontrib/gstreamereditor/util.py
+++ keyecontrib/gstreamereditor/util.py
@@ -11,7 +11,7 @@ class gst_plugs_loader():
         if gst_plugs_loader.table_default is None:
             pth_csv = os.path.join(os.path.split(__file__)[0], 'datas/deepstream5_gstreamer_plugins_20230529_032323_.csv')
             gst_plugs_loader.table_default = pd.read_csv(pth_csv)
-        return gst_plugs_loader.table_default
+        return gst_plugs_loader.table_default.sort_values('title')
 
 
 if __name__ == '__main__':
